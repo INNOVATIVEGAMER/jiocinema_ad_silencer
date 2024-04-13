dafdb2f651e81e6f2c519763a1efd7c8988ff5b2
Prasad Dilip Patewar <prasadpatewar39@gmail.com>
2024-04-13 23:38:31 +0530

    first commit with all required code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# jiocinema_ad_muter
-This extension mutes the player as ad starts and unmute as ad ends
+# Goal
+
+This extension mutes the player as ad starts and unmute as ad ends on jiocinema
+Specifically build for jiocinema for IPL 2024 ads
+
+## Compatibilty
+
+Chromium based browers
+
+## Contributing Guidelines
+
+This is a simple around 50 line script. But you have any better approach please raise a issue so we can discuss it.
+
+## Contributing
+
+1. Clone the repo in your local system
+2. Open your Chrome browser and navigate to chrome://extensions.
+3. Enable Developer Mode by clicking the toggle switch next to Developer mode.
+4. Click the Load unpacked button and select the extension directory by extracting given zip file.
+5. Make the changes in extension code and reload the extension to see the reflected changes
---
+++ content.js
@@ -0,0 +1,49 @@
+const MUTE_BTN_SELECTOR =
+  "#testing > div.mui-style-jshu1n-buttonPanel > div.mui-style-bizjqu-buttonControlsLeft > div > button";
+
+const AD_DIV_SELECTOR =
+  "#main > div.hls-player-wrapper.mui-style-7dmsrx-root > div > div.mui-style-5kj990-adTag";
+
+const adDetector = () => {
+  const muteBtn = document.querySelector(MUTE_BTN_SELECTOR);
+  if (!muteBtn) return false;
+
+  if (muteBtn.ariaLabel == "Unmute") muteBtn.click();
+
+  let adStarted = false;
+
+  setInterval(() => {
+    const adDiv = document.querySelector(AD_DIV_SELECTOR);
+
+    if (adDiv && !adStarted) {
+      adStarted = true;
+      muteBtn.click();
+    }
+  }, 500);
+
+  let adFinishCheck = false;
+  setInterval(() => {
+    const adDiv = document.querySelector(AD_DIV_SELECTOR);
+
+    if (!adDiv && adStarted && !adFinishCheck) {
+      adFinishCheck = true;
+      setTimeout(() => {
+        adFinishCheck = false;
+
+        const adDiv = document.querySelector(AD_DIV_SELECTOR);
+
+        if (!adDiv) {
+          adStarted = false;
+          muteBtn.click();
+        }
+      }, 8000);
+    }
+  }, 500);
+
+  return true;
+};
+
+const it = setInterval(() => {
+  const isLoaded = adDetector();
+  if (isLoaded) clearInterval(it);
+}, 2000);
